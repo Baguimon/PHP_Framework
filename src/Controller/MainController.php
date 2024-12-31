cd820076ffb18dedc8985de15f4a7457c0d55634
Baguimon <timothe.winkler@gmail.com>
2024-12-31 11:19:23 +0100

    1er controleur et début des routeurs

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController{
+
+    #[Route('/')]
+    public function test () {
+
+        return $this->render('main.html.twig',);
+    }
+
+    #[Route('/contact')]
+    public function contact () {
+
+        return $this->render('contact.html.twig');
+    }
+
+}
+
+?>
