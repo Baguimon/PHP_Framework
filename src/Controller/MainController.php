d4d222265f123805e95bf96d4d4c4f2cd18e96d2
Baguimon <timothe.winkler@gmail.com>
2024-12-31 11:38:03 +0100

    Routing Upgrade

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,13 +7,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController{
 
-    #[Route('/')]
+    #[Route('/', name: 'app_main')]
     public function test () {
 
         return $this->render('main.html.twig',);
     }
 
-    #[Route('/contact')]
+    #[Route('/nous-contacter', name: 'app_contact')]
     public function contact () {
 
         return $this->render('contact.html.twig');
